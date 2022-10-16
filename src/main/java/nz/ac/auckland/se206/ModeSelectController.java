5f45983da66040d59c61cb73cae9a1652df0d0da
Ben Zhou <bzho257@aucklanduni.ac.nz>
2022-10-16 18:59:55 +1300

    feat: added tool tips for game modes

--- src/main/java/nz/ac/auckland/se206/ModeSelectController.java
+++ src/main/java/nz/ac/auckland/se206/ModeSelectController.java
@@ -5,11 +5,11 @@ import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Tooltip;
 import javafx.scene.text.Font;
+import javafx.util.Duration;
 
 public class ModeSelectController {
-
-  @FXML private CheckBox checkBoxHidWordMode;
   private static ActionEvent event;
 
   /**
@@ -21,12 +21,59 @@ public class ModeSelectController {
     return event;
   }
 
+  @FXML private CheckBox checkBoxHidWordMode;
+  @FXML private Button zenModeButton;
+  @FXML private Button normalModeButton;
+  private Tooltip zenModeMessage;
+  private Tooltip normalModeMessage;
+  private Tooltip hiddenModeMessage;
+
   /**
    * This method is called upon first load of the fxml. It sets the font to the custom IndieFlower
-   * font.
+   * font. It also sets the tooltips for the game modes.
    */
   public void initialize() {
     Font.loadFont(App.class.getResourceAsStream("/fonts/IndieFlower-Regular.ttf"), 100);
+
+    zenModeMessage = new Tooltip();
+    normalModeMessage = new Tooltip();
+    hiddenModeMessage = new Tooltip();
+
+    // Sets the display message hover to appear after 100ms
+    zenModeMessage.setShowDelay(Duration.millis(100));
+    // Sets the display message hover to appear after 100ms
+    normalModeMessage.setShowDelay(Duration.millis(100));
+    // Sets the display message hover to appear after 100ms
+    hiddenModeMessage.setShowDelay(Duration.millis(100));
+
+    // Sets the display message font styles
+    zenModeMessage.setStyle("-fx-font-size: 16px; -fx-font-family: 'Indie Flower'");
+    // Sets the display message font styles
+    normalModeMessage.setStyle("-fx-font-size: 16px; -fx-font-family: 'Indie Flower'");
+    // Sets the display message font styles
+    hiddenModeMessage.setStyle("-fx-font-size: 16px; -fx-font-family: 'Indie Flower'");
+    // Sets the text for zen mode
+    zenModeMessage.setText(
+        "The same as normal game mode, but there is no time limit and user can use different colours."
+            + System.lineSeparator()
+            + "Zen mode games are not tracked.");
+    // Sets the text for normal mode
+    normalModeMessage.setText(
+        "Draw the given word within the time limit to win."
+            + System.lineSeparator()
+            + "Games are tracked in user's statistics.");
+    // Sets the text for hidden mode
+    hiddenModeMessage.setText(
+        "Additional option for normal and zen mode."
+            + System.lineSeparator()
+            + "Instead of giving words, the word definition is given to the user."
+            + System.lineSeparator()
+            + "Hidden mode games are not tracked");
+
+    // Adding tooltip to respective buttons/checkbox
+    Tooltip.install(zenModeButton, zenModeMessage);
+    Tooltip.install(normalModeButton, normalModeMessage);
+    Tooltip.install(checkBoxHidWordMode, hiddenModeMessage);
   }
 
   /**
@@ -81,5 +128,11 @@ public class ModeSelectController {
 
     // Sets the scene to the game menu
     currentScene.setRoot(SceneManager.getUiRoot(SceneManager.AppScene.GAME));
+  }
+
+  /** This method plays the pencil write sound. */
+  @FXML
+  private void onPlayWriteSound() {
+    AudioController.playPencilWrite();
   }
 }
